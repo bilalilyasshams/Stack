7c6a341280a7166159e7785be4efdeabae95e65f
Bilal_Shams <146247915+bilalilyasshams@users.noreply.github.com>
2024-10-06 00:55:47 +0500

    fixed some logical errors

--- task1.cpp
+++ task1.cpp
@@ -3,15 +3,15 @@
 using namespace std;
 int prec(char c)
 {
-    if(c=='^')
+    if (c == '^')
     {
         return 3;
     }
-    else if(c=='*' || c=='/')
+    else if (c == '*' || c == '/')
     {
         return 2;
     }
-    else if(c=='+', c=='-')
+    else if (c == '+'|| c == '-')
     {
         return 1;
     }
@@ -25,47 +25,47 @@ string infixtopostfix(string s)
 {
     stack<char> st;
     string res;
-    for (int i=0; i<=s.length(); i++)
+    for (int i = 0; i < s.length(); i++)
     {
-        if ((s[i]>='a' && s[i]<='z') || (s[i]>='A' && s[i]<='Z'))
+        if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
         {
-            res+=s[i];
+            res += s[i];
         }
-        else if(s[i]=='(')
+        else if (s[i] == '(')
         {
             st.push(s[i]);
         }
-        else if(s[i]==')')
+        else if (s[i] == ')')
         {
-            while (!st.empty() && st.top()!='(')
+            while (!st.empty() && st.top() != '(')
             {
-                res+=st.top();
+                res += st.top();
                 st.pop();
             }
-            if(!st.empty())
+            if (!st.empty())
             {
                 st.pop();
             }
         }
         else
         {
-            while (!st.empty() && prec(st.top())>prec(s[i]))
+            while (!st.empty() && prec(st.top()) >= prec(s[i]))
             {
-                res+=st.top();
+                res += st.top();
                 st.pop();
             }
             st.push(s[i]);
         }
-
     }
     while (!st.empty())
     {
-        res+=st.top();
+        res += st.top();
         st.pop();
     }
     return res;
 }
 int main()
 {
-   cout<<infixtopostfix("(a-b/c)*(a/k-l)");
+    cout << infixtopostfix("A+B*C+D")<<endl;
+    cout << infixtopostfix("(A+B)*(C+D)");
 }
